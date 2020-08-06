47daa3c5a8903d0bfa8306a1460676dc680de83f
lestercardoz11 <lestercardoz11@gmail.com>
2020-08-07 00:10:37 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
 import './styles/App.css';
+import logo from './logo.svg';
+import mode from './img/mode.png';
+import avatar from './img/avatar.svg';
+import Typical from 'react-typical';
+
 
 function App() {
 return (
 <div className="App">
   <header className="App-header">
-    <div className="main-header-content">
-      <h1>Hi, I'm Lester Cardoz.</h1>
-      <h2>I'm a web developer.</h2>
+    <div className="navbar">
+          <a className="sitehead"><img src={logo} className="App-logo" alt="logo" /></a>
+          <ul>
+              <li><a>Home</a></li>
+              <li><a>About</a></li>
+              <li><a>Contact</a></li>
+              <li><a><img src={mode} className="mode-logo" alt="mode" /></a></li>
+          </ul>
+      </div>
+      <div className="main-header-content">
+      <div>
+        <img src={avatar} className="Avatar" alt="avatar" />
+      </div>
+      <div className="heading">
+        <h1>Hi, I'm Lester Cardoz.</h1>
+        <h2>I'm a {' '}
+          <Typical 
+          loop={Infinity}
+          wrapper="b"
+          steps={[
+            'Web Developer.',
+            1000,
+            'Technology Enthusiast.',
+            1000,
+            'Footballer.',
+            1000
+          ]}
+          />
+        </h2>
+        </div>
     </div>
     <div className="ocean">
       <div className="wave"></div>
