78c4d1bafe7dfbd7c65b2264b55572f28983be0f
lestercardoz11 <lestercardoz11@gmail.com>
2021-07-13 16:09:36 +0000

    Updates

---
+++ precache-manifest.ef188aded5c3900928fc6fb3c77b489a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b71a786393031ca5026834d50ef21f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c17aab72eedd4a71f9",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "719707bca8db0449b64a",
+    "url": "/static/css/main.e78c9ee0.chunk.css"
+  },
+  {
+    "revision": "28c17aab72eedd4a71f9",
+    "url": "/static/js/2.5222a0f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5222a0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719707bca8db0449b64a",
+    "url": "/static/js/main.5c4509bb.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "42506934830e393664a177be3d9a5134",
+    "url": "/static/media/DP-large.42506934.png"
+  },
+  {
+    "revision": "13540205131be27ffdfe81022dd61535",
+    "url": "/static/media/avatar.13540205.svg"
+  },
+  {
+    "revision": "b27ec758df1964207f37d15d72603a26",
+    "url": "/static/media/bg-img.b27ec758.svg"
+  },
+  {
+    "revision": "d95b9cc5be0f42c59cf38aae3d414b07",
+    "url": "/static/media/bootstrap.d95b9cc5.svg"
+  },
+  {
+    "revision": "1fa13794afa7fc4a2c3d4d49cdf4ad42",
+    "url": "/static/media/cplusplus.1fa13794.svg"
+  },
+  {
+    "revision": "7e991e2aba57be7857806f952c38c8b1",
+    "url": "/static/media/csharp.7e991e2a.svg"
+  },
+  {
+    "revision": "0aa19834847fc82296fcbc5cc2b81473",
+    "url": "/static/media/css3.0aa19834.svg"
+  },
+  {
+    "revision": "b31273353438ae02891b87f36802ecd6",
+    "url": "/static/media/github.b3127335.svg"
+  },
+  {
+    "revision": "9291bd092a2f605e6c6675358a76c2e3",
+    "url": "/static/media/heroku.9291bd09.svg"
+  },
+  {
+    "revision": "063ce28a1b7c61bc60f19e1e97f7487f",
+    "url": "/static/media/html5.063ce28a.svg"
+  },
+  {
+    "revision": "ee491c0f6c5be768358fdce06f4a42bc",
+    "url": "/static/media/java.ee491c0f.svg"
+  },
+  {
+    "revision": "20ee3998d9cd38aa4d1be40722262337",
+    "url": "/static/media/javascript.20ee3998.svg"
+  },
+  {
+    "revision": "71e98dc5e050de21e190a12ff4feb689",
+    "url": "/static/media/jquery.71e98dc5.svg"
+  },
+  {
+    "revision": "efeae1331f57213284f518295e315653",
+    "url": "/static/media/logo.efeae133.svg"
+  },
+  {
+    "revision": "3b79e65db493ac90870827bc019f184e",
+    "url": "/static/media/matlab.3b79e65d.svg"
+  },
+  {
+    "revision": "2fefb95a08ea2999da865df686c1d6af",
+    "url": "/static/media/mysql.2fefb95a.svg"
+  },
+  {
+    "revision": "494fd49578782b563c94be3fce156be6",
+    "url": "/static/media/npm.494fd495.svg"
+  },
+  {
+    "revision": "eea4bc581bd10e6f0fb49d35fb16430d",
+    "url": "/static/media/php.eea4bc58.svg"
+  },
+  {
+    "revision": "eb70d4aa1b1834ff79dd99a21e5f6c6b",
+    "url": "/static/media/project1.eb70d4aa.png"
+  },
+  {
+    "revision": "0c886491cdbbcce6c305cf775074f89e",
+    "url": "/static/media/project2.0c886491.png"
+  },
+  {
+    "revision": "c8b49f5da00e77e17c6785811769fc35",
+    "url": "/static/media/project3.c8b49f5d.png"
+  },
+  {
+    "revision": "36e11cc5dce099d2098ae3a69d2d6411",
+    "url": "/static/media/project4.36e11cc5.png"
+  },
+  {
+    "revision": "5357551a078588ceda82aaf4a69f9229",
+    "url": "/static/media/python.5357551a.svg"
+  },
+  {
+    "revision": "6b96a3ac9406b50cae1f84da074d0a1b",
+    "url": "/static/media/react.6b96a3ac.svg"
+  },
+  {
+    "revision": "5d1ce41290e59bc5cef07b7cda8a04b5",
+    "url": "/static/media/sass.5d1ce412.svg"
+  },
+  {
+    "revision": "b0d5c79b91f23e3785d24a077319cd28",
+    "url": "/static/media/sqlserver.b0d5c79b.svg"
+  },
+  {
+    "revision": "28125f0f031ddb1a2259e9b684ec1765",
+    "url": "/static/media/vscode.28125f0f.svg"
+  },
+  {
+    "revision": "a1c386018884fe675c77917113739bdb",
+    "url": "/static/media/wordpress.a1c38601.svg"
+  }
+]);
